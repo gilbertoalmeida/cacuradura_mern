520b4e37c0a1c73e34c4f62e0362b1d4df925aee
Gilberto Almeida <almeidanetogilberto@gmail.com>
2019-10-31 11:10:14 +0100

    Added routing to react, created a single general article page and a NotFound page

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,13 @@
-import React, { Component } from "react";
+import React, { Fragment, Component } from "react";
 import { Provider } from "react-redux";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import store from "./store";
 
-import ArticleFeed from "./components/ArticleFeed";
+import HomePage from "./components/HomePage";
+import Routes from "./components/routing/Routes";
+
+/* For when I want to translate the app:
+https://medium.com/@ryandrewjohnson/adding-multi-language-support-to-your-react-redux-app-cf6e64250050 */
 
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
@@ -11,19 +16,16 @@ class App extends Component {
   render() {
     return (
       <Provider store={store}>
-        <div className="App">
-          <header className="App-header">
-            <h1>
-              Cacura<span>Dura</span>
-            </h1>
-            <p id="maindescription">
-              Cacuras em formação
-              <br />
-              Acompanhe essa metamorfose
-            </p>
-          </header>
-          <ArticleFeed />
-        </div>
+        <Router>
+          <Fragment>
+            <div className="App">
+              <Switch>
+                <Route exact path="/" component={HomePage} />
+                <Route component={Routes} />
+              </Switch>
+            </div>
+          </Fragment>
+        </Router>
       </Provider>
     );
   }
