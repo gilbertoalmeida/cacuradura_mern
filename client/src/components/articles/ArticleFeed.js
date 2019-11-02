eee414418da042f5229500f068dd761bd376518e
Gilberto Almeida <almeidanetogilberto@gmail.com>
2019-11-02 10:54:56 +0100

    displayed the article feed and the article page nicer with the info of the articles from the database. Linked the articles from the feed to the single page articles

--- client/src/components/articles/ArticleFeed.js
+++ client/src/components/articles/ArticleFeed.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import { Container, ListGroup, ListGroupItem } from "reactstrap";
 import { connect } from "react-redux";
+import { Link } from "react-router-dom";
 import { getArticles } from "../../actions/articleActions";
 import PropTypes from "prop-types";
 
@@ -19,8 +20,32 @@ class ArticleFeed extends Component {
     return (
       <Container>
         <ListGroup>
-          {articles.map(({ _id, title }) => (
-            <ListGroupItem>{title}</ListGroupItem>
+          {articles.map(({ _id, title, date, author, body }) => (
+            <ListGroupItem>
+              <div className="main-box-element">
+                <Link to={`/articles/${_id}`} className="article-title link">
+                  {title}
+                </Link>
+                <time datetime={date}>
+                  <b>
+                    §}> {date}, por {author.username}
+                  </b>
+                </time>
+                <Link className="link" to={`/articles/${_id}`}>
+                  <img
+                    src="/Assets/a-cacurice-vem.png"
+                    alt="Foto de um girassol murcho com o scripting de'A cacurice vem' por cima"
+                  ></img>
+                </Link>
+                <br />
+                <p>
+                  {body + " "}
+                  <Link to={`/articles/${_id}`} className="link">
+                    [Leia mais]
+                  </Link>
+                </p>
+              </div>
+            </ListGroupItem>
           ))}
         </ListGroup>
       </Container>
