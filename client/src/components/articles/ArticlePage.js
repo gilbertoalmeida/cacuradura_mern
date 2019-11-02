95bc764d4becee9a210af787aae629e2a3de1680
Gilberto Almeida <almeidanetogilberto@gmail.com>
2019-11-02 10:54:56 +0100

    displayed the article feed and the article page nicer with the info of the articles from the database. Linked the articles from the feed to the single page articles

--- client/src/components/articles/ArticlePage.js
+++ client/src/components/articles/ArticlePage.js
@@ -1,5 +1,6 @@
 import React, { Fragment, useEffect } from "react";
 import { connect } from "react-redux";
+import { Link } from "react-router-dom";
 import { getArticle } from "../../actions/articleActions";
 import PropTypes from "prop-types";
 
@@ -10,9 +11,31 @@ const ArticlePage = ({ getArticle, article: { article, loading }, match }) => {
 
   //ver o que acontece quando chanma essa página com um id que nao existe
 
-  return (
+  return loading || article === null ? (
+    <h1>Loading</h1>
+  ) : (
     <Fragment>
-      <h1>Página de um artigo</h1>
+      <h1>
+        um <span>artigo</span>
+      </h1>
+      <div className="main-box-element">
+        <Link to={`/articles/${article._id}`} className="article-title link">
+          {article.title}
+        </Link>
+        <time datetime={article.date}>
+          <b>
+            §}> {article.date}, por {article.author.username}
+          </b>
+        </time>
+        <Link className="link" to={`/articles/${article._id}`}>
+          <img
+            src="/Assets/a-cacurice-vem.png"
+            alt="Foto de um girassol murcho com o scripting de'A cacurice vem' por cima"
+          ></img>
+        </Link>
+        <br />
+        <p>{article.body}</p>
+      </div>
     </Fragment>
   );
 };
