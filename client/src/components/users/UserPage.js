0a94b6273c45311516a5a1b7ee85d654d6fc8a09
Gilberto Almeida <almeidanetogilberto@gmail.com>
2020-02-11 19:39:14 +0100

    Now I am displaying all the user's articles in their pages like the feed, with the images and titles only

--- client/src/components/users/UserPage.js
+++ client/src/components/users/UserPage.js
@@ -1,29 +1,14 @@
 import React, { Fragment, useEffect, useState } from "react";
 import { connect } from "react-redux";
 import { Link } from "react-router-dom";
-import {
-  TabContent,
-  TabPane,
-  Nav,
-  NavItem,
-  NavLink,
-  Card,
-  CardTitle,
-  CardText,
-  Row,
-  Col,
-  Container,
-  ListGroup,
-  ListGroupItem
-} from "reactstrap";
+import { TabContent, TabPane, Nav, NavItem, NavLink } from "reactstrap";
 import classnames from "classnames";
 import { getUser } from "../../actions/userActions";
 import { getUserArticles } from "../../actions/articleActions";
 import PropTypes from "prop-types";
-
 import { withLocalize, Translate } from "react-localize-redux";
 
-import ReactHtmlParser from "react-html-parser";
+import UserArticleFeed from "./UserArticleFeed.js";
 
 const UserPage = ({
   getUser,
@@ -94,23 +79,7 @@ const UserPage = ({
           </Link>
         )}
 
-      <header className="App-header">
-        <h1>
-          <Translate id="header.a(cacura)"></Translate>
-          <span>cacura</span>
-        </h1>
-        <h2>
-          <Translate id="header.still_working"></Translate>
-        </h2>
-      </header>
       <div className="main-box-element">
-        <Link to={`/users/${loadedUser._id}`} className="article-title link">
-          <Translate
-            id="user_page.owner_phrase"
-            data={{ owner: loadedUser.username }}
-          ></Translate>
-        </Link>
-        <br />
         <Nav tabs className="justify-content-center user-nav-tabs">
           <NavItem className="user-nav-item">
             <NavLink
@@ -122,20 +91,10 @@ const UserPage = ({
               <Translate id="user_page.articles"></Translate>
             </NavLink>
           </NavItem>
-          <NavItem>
-            <NavLink
-              className={classnames({ active: activeTab === "2" })}
-              onClick={() => {
-                toggle("2");
-              }}
-            >
-              <Translate id="user_page.pictures"></Translate>
-            </NavLink>
-          </NavItem>
         </Nav>
         <TabContent activeTab={activeTab}>
           <TabPane tabId="1">
-            <Container>
+            <Fragment>
               {articles.length === 0 ? (
                 <header>
                   <h2> {"><((((º>"}</h2>
@@ -145,75 +104,9 @@ const UserPage = ({
                   </h2>
                 </header>
               ) : (
-                <>
-                  <header>
-                    <h2>
-                      <Translate id="user_page.article_design" />{" "}
-                    </h2>
-                  </header>
-
-                  <ListGroup>
-                    {articles.map(({ _id, title, date, author, body }) => (
-                      <ListGroupItem
-                        key={_id}
-                        className="article-list-group-item"
-                      >
-                        <div>
-                          <Link
-                            to={`/articles/${_id}`}
-                            className="article-title link"
-                          >
-                            <h3 className="article-title">{title}</h3>
-                          </Link>
-                          <time dateTime={date}>
-                            §}>{" "}
-                            {new Date(date).getDate() +
-                              "/" +
-                              (new Date(date).getMonth() + 1) +
-                              "/" +
-                              new Date(date).getFullYear()}
-                            <Translate id="article.by"></Translate>{" "}
-                            <Link
-                              to={`/users/${author._id}`}
-                              className="user-link link"
-                            >
-                              {author.username}
-                            </Link>
-                          </time>
-                          <br />
-                          <br />
-                          <div className="article-body">
-                            {ReactHtmlParser(body)}
-                          </div>
-                        </div>
-                      </ListGroupItem>
-                    ))}
-                  </ListGroup>
-                </>
+                <UserArticleFeed loadedUserID={loadedUser._id} />
               )}
-            </Container>
-          </TabPane>
-          <TabPane tabId="2">
-            <Row>
-              <Col sm="6">
-                <Card body className="pic-card">
-                  <CardTitle>Primeira foto</CardTitle>
-                  <CardText>
-                    Uma foto pretenciosa com um leve filtro vermelho, tipo a que
-                    tá nos artigos.
-                  </CardText>
-                </Card>
-              </Col>
-              <Col sm="6">
-                <Card body className="pic-card">
-                  <CardTitle>Segunda foto</CardTitle>
-                  <CardText>
-                    Um nudez artístico. <br></br> SIM! <br></br> Será permitida
-                    nudez aqui <br></br> mORRE INSTAGRAM!
-                  </CardText>
-                </Card>
-              </Col>
-            </Row>
+            </Fragment>
           </TabPane>
         </TabContent>
       </div>
