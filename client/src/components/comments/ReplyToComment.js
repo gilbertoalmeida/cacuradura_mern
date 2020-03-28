8a645187417d62326608d6fcc3a67ccc382b2d69
Gilberto Almeida <almeidanetogilberto@gmail.com>
2020-03-28 14:30:54 +0100

    Added the replay input to the comments that appears through a toogle

---
+++ client/src/components/comments/ReplyToComment.js
@@ -0,0 +1,112 @@
+import React, { Fragment, useState, useEffect } from "react";
+import { Button, Form, FormGroup, Input, Alert } from "reactstrap";
+import { withLocalize, Translate } from "react-localize-redux";
+import { clearErrors } from "../../actions/errorActions";
+import { connect } from "react-redux";
+import PropTypes from "prop-types";
+
+const ReplyToComment = ({
+  articleID,
+  auth: { loggedUser },
+  comment,
+  error,
+  clearErrors
+}) => {
+  const [commentValue, setCommentValue] = useState("");
+
+  useEffect(() => {
+    clearErrors();
+  }, [clearErrors, loggedUser]);
+
+  const onSubmit = e => {
+    e.preventDefault();
+
+    const newComment = {
+      articleID,
+      author: {
+        username: loggedUser.username,
+        _id: loggedUser._id,
+        picture: loggedUser.profile_pictures[0]
+      },
+      comment: commentValue
+    };
+
+    console.log(newComment);
+  };
+
+  return loggedUser ? (
+    <Fragment>
+      <div>
+        <Form onSubmit={onSubmit} className="add-reply-form">
+          <FormGroup>
+            <Translate>
+              {({ translate }) => (
+                <Input
+                  type="input"
+                  name="username"
+                  id="username"
+                  value={commentValue}
+                  placeholder={translate("comments_section.input")}
+                  onChange={e => {
+                    setCommentValue(e.target.value);
+                  }}
+                  className="add-reply-form__input"
+                />
+              )}
+            </Translate>
+          </FormGroup>
+          {error.msg.msg ? (
+            <Translate>
+              {({ translate }) => (
+                <Alert
+                  style={{ width: "90%", margin: "10px auto" }}
+                  color="add-reply-alert alert-danger"
+                >
+                  {translate(`error_messages.${error.msg.msg}`)}
+                </Alert>
+              )}
+            </Translate>
+          ) : null}
+          {/* operator to show the alert only is there is an error */}
+          <div className="add-reply-form__submit-button-container">
+            {comment.posting_failed ? (
+              <Button className="add-reply-form__submit-button-failed">
+                <Translate id="comments_section.posting_failed" />
+              </Button>
+            ) : (
+              <Button className="add-reply-form__submit-button">
+                {comment.posting ? (
+                  <Translate id="comments_section.posting" />
+                ) : (
+                  <Translate id="comments_section.submit_button" />
+                )}
+              </Button>
+            )}
+          </div>
+        </Form>
+      </div>
+    </Fragment>
+  ) : (
+    ""
+  );
+};
+
+ReplyToComment.propTypes = {
+  auth: PropTypes.object.isRequired,
+  comment: PropTypes.object.isRequired,
+  error: PropTypes.object.isRequired,
+  clearErrors: PropTypes.func.isRequired
+};
+
+const mapStateToProps = state => ({
+  auth: state.auth,
+  comment: state.comment,
+  error: state.error
+});
+
+export default withLocalize(
+  connect(
+    mapStateToProps,
+    { clearErrors }
+  )(ReplyToComment)
+);
