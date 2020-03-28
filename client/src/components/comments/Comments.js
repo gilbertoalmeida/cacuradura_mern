46eff7e827ba912d7ec0f381f64df2243f256274
Gilberto Almeida <almeidanetogilberto@gmail.com>
2020-03-28 14:30:54 +0100

    Added the replay input to the comments that appears through a toogle

--- client/src/components/comments/Comments.js
+++ client/src/components/comments/Comments.js
@@ -1,15 +1,30 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { connect } from "react-redux";
 import PropTypes from "prop-types";
 import { withLocalize, Translate } from "react-localize-redux";
 import { Link } from "react-router-dom";
 import { getComments } from "../../actions/commentActions";
 import { prettyDateHours } from "../../Utils/Utils";
+import ReplyToComment from "./ReplyToComment";
+import { Collapse } from "reactstrap";
 
-const Comments = ({ getComments, comment: { comments, loading }, match }) => {
+const Comments = ({
+  getComments,
+  comment: { comments, loading },
+  auth: { loggedUser },
+  match
+}) => {
   useEffect(() => {
     getComments(match.params.id);
   }, [getComments, match.params.id]);
+
+  const [commentIDWithActiveReply, setCommentIDWithActiveReply] = useState("");
+
+  const showReplyInput = commentID => {
+    let activeReplyID =
+      commentIDWithActiveReply === commentID ? "null" : commentID;
+    setCommentIDWithActiveReply(activeReplyID);
+  };
 
   return (
     <div className="comments-section">
@@ -18,26 +33,48 @@ const Comments = ({ getComments, comment: { comments, loading }, match }) => {
         <div>loading</div>
       ) : (
         <div className="comments-section__comments">
-          {comments.map(({ _id, author, posted, comment_text }) => (
-            <div key={_id} className="comments-section__comments__single">
-              <div className="comments-section__comments__single__img">
-                <img
-                  src={
-                    author.picture.length === 0
-                      ? "/Assets/no_profile_pic.png"
-                      : author.picture
-                  }
-                  alt="profile of the user"
-                />
-                <div className="comments-section__comments__single__img-filter"></div>
+          {Object.keys(comments).map((key, index) => (
+            <div key={index} className="comments-section__comments__single">
+              <div className="comments-section__comments__single__content">
+                <Link to={`/users/${comments[key].author._id}`}>
+                  <div className="comments-section__comments__single__content__img">
+                    <img
+                      src={
+                        comments[key].author.picture.length === 0
+                          ? "/Assets/no_profile_pic.png"
+                          : comments[key].author.picture
+                      }
+                      alt="profile of the user"
+                    />{" "}
+                    <div className="comments-section__comments__single__content__img-filter"></div>
+                  </div>
+                </Link>
+                <div className="comments-section__comments__single__content__text">
+                  <Link
+                    to={`/users/${comments[key].author._id}`}
+                    className="user-link link"
+                  >
+                    {comments[key].author.username}
+                  </Link>{" "}
+                  {prettyDateHours(comments[key].posted)}
+                  <div>{comments[key].comment_text}</div>
+                </div>
               </div>
-              <div className="comments-section__comments__single__text">
-                <Link to={`/users/${author._id}`} className="user-link link">
-                  {author.username}
-                </Link>{" "}
-                {prettyDateHours(posted)}
-                <div>{comment_text}</div>
-              </div>
+              {loggedUser ? (
+                <div
+                  onClick={() => {
+                    showReplyInput(comments[key]._id);
+                  }}
+                >
+                  Reply
+                </div>
+              ) : (
+                ""
+              )}
+
+              <Collapse isOpen={commentIDWithActiveReply === comments[key]._id}>
+                <ReplyToComment />
+              </Collapse>
             </div>
           ))}
         </div>
@@ -48,11 +85,13 @@ const Comments = ({ getComments, comment: { comments, loading }, match }) => {
 
 Comments.propTypes = {
   getComments: PropTypes.func.isRequired,
-  comment: PropTypes.object.isRequired
+  comment: PropTypes.object.isRequired,
+  auth: PropTypes.object.isRequired
 };
 
 const mapStateToProps = state => ({
-  comment: state.comment
+  comment: state.comment,
+  auth: state.auth
 });
 
 export default withLocalize(
@@ -61,3 +100,7 @@ export default withLocalize(
     { getComments }
   )(Comments)
 );
+
+/* { _id, author, posted, comment_text } */
+
+/* answerWithReplyInput === comments[key]._id */
